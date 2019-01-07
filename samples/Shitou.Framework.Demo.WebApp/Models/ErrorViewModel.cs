14a4c720803a43d312680633c5628e592ee73767
yanglb <yanglb@fxtcn.com>
2019-01-07 18:40:49 +0800

    添加appweb

---
+++ samples/Shitou.Framework.Demo.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shitou.Framework.Demo.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
